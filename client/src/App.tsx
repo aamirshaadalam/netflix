d5ae5d1fda8efbc1f502409a60df7ee8325c5f1a
Aamir Shaad Alam <aamirshaad.alam@gmail.com>
2024-12-27 15:06:42 -0500

    folder restructure

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.scss';
+// import Login from './pages/login/Login';
+// import Register from './pages/register/Register';
+// import Watch from './pages/watch/Watch';
+import Home from './pages/home/Home';
+
+function App() {
+  return (
+    <div className='app'>
+      <Home />
+      {/* <Login /> */}
+      {/* <Watch /> */}
+      {/* <Register /> */}
+    </div>
+  );
+}
+
+export default App;
