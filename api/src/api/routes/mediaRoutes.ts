3d949e87dece324625a91950811ee8eb3ca187a2
Aamir Shaad Alam <aamirshaad.alam@gmail.com>
2025-01-05 21:38:57 -0500

    update route

--- api/src/api/routes/mediaRoutes.ts
+++ api/src/api/routes/mediaRoutes.ts
@@ -4,7 +4,7 @@ import { validateCreateMedia, validateGetAllMedia, validateUpdateMedia } from '.
 
 const router = Router();
 router.get('/', validateGetAllMedia, mediaController.getAllMedia);
-router.get('/:id', mediaController.getMedia);
+router.get('/find/:id', mediaController.getMedia);
 router.get('/featured', mediaController.getFeaturedMedia);
 router.post('/', validateCreateMedia, mediaController.createMedia);
 router.put('/:id', validateUpdateMedia, mediaController.updateMedia);
